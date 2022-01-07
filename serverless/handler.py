d5f7c0be1e90190226c198327497c4c90fe23d34
Isaac Gan <ganwancheng@gmail.com>
2022-01-07 18:39:56 +0800

    change handler

--- serverless/handler.py
+++ serverless/handler.py
@@ -7,6 +7,7 @@ def process_audio(event, context):
         "message": "Go Serverless v2.0! Your function executed successfully!",
         "input": event,
     }
+    print(type(event["body"]))
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
