606efccceced36a4d178a168a929faeb7ac5fc12
Joven Pua Zai Xiong <jovenpua51@gmail.com>
2022-01-07 20:32:28 +0800

    add voice file handler (incomplete)

--- client/main.py
+++ client/main.py
@@ -1,6 +1,8 @@
 import os
 import telebot
+import emoji
 from dotenv import load_dotenv
+from telebot import types
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
@@ -12,4 +14,22 @@ def send_greeting(message):
     bot.send_message(message.chat.id, "Hello! I can change the voice and add various effects to it")
 
 
-bot.polling()
+@bot.message_handler(content_types=['audio', 'voice'])
+def handle_voice_file(message):
+
+    bot.send_voice(message.chat.id, message.voice.file_id)
+    
+    # add buttons for the user to select the audio effect he/she desires
+    markup = types.ReplyKeyboardMarkup()
+
+    itemPitchIncr = types.KeyboardButton('Pitch Up')
+    itemPitchDecr = types.KeyboardButton('Pitch Down')
+
+    markup.row(itemPitchIncr, itemPitchDecr)
+
+    bot.send_message(message.chat.id, "Choose the audio effect you would like to add", reply_markup=markup)
+
+    # depending on audio effect selection, send the processed voice file back to user
+    
+
+bot.infinity_polling()
